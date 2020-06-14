bef3b97b3192f245f0e5ef7eae7ccdc0e8085cce
Roger Gunreben <ruediger.gunreben@isarnet.de>
2020-06-14 06:54:58 +0200

    removed AFTER_SUCCESS variables

--- jobService/src/test/java/de/gunis/roger/jobService/WithPostProcessing.java
+++ jobService/src/test/java/de/gunis/roger/jobService/WithPostProcessing.java
@@ -1,12 +1,14 @@
 package de.gunis.roger.jobService;
 
-import org.junit.Ignore;
+import de.gunis.roger.jobService.imports.CsvFileLoader;
+import de.gunis.roger.jobService.jobsToDo.JobDescription;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Properties;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
 
 public class WithPostProcessing {
 
@@ -18,13 +20,22 @@ public class WithPostProcessing {
         String folderWithConverterScripts = new File(this.getClass().getClassLoader().getResource("WithPostProcessing/bin/ics2html")
                 .getFile()).getParentFile().toString();
 
-        Properties props = new Properties();
         String inputDataPath = "WithPostProcessing/inputData/";
-        String configFilePath = this.getClass().getClassLoader().getResource(inputDataPath + "AFTER_SUCCESS_VARIABLES.txt").getFile();
-        props.load(new FileReader(configFilePath));
+        String jobDescriptionFilePath = this.getClass().getClassLoader().getResource(inputDataPath + "JobDescription.csv").getFile();
 
-        System.setProperty("PDF_START", props.getProperty("PDF_START"));
-        System.setProperty("PDF_END", props.getProperty("PDF_END"));
+        ZoneId zoneId = ZoneId.systemDefault();
+        CsvFileLoader csvFileLoader = new CsvFileLoader("dd.MM.yyyy");
+        LocalDate begin = csvFileLoader.importJobDescriptionFromFile(jobDescriptionFilePath).stream().map(JobDescription::getBegin)
+                .sorted(Comparator.comparingLong(i -> i.atStartOfDay(zoneId).toEpochSecond()))
+                .findFirst().get();
+
+        LocalDate end = csvFileLoader.importJobDescriptionFromFile(jobDescriptionFilePath).stream().map(JobDescription::getEnd)
+                .sorted(Comparator.comparingLong(i -> i.atStartOfDay(zoneId).toEpochSecond()))
+                .sorted(Comparator.reverseOrder())
+                .findFirst().get();
+
+        System.setProperty("PDF_START", begin.toString());
+        System.setProperty("PDF_END", end.toString());
 
         String ics2html = this.getClass().getClassLoader().getResource("WithPostProcessing/bin/ics2html").getFile();
         String holidays = this.getClass().getClassLoader().getResource(inputDataPath + "Holidays.csv").getFile();
@@ -47,33 +58,32 @@ public class WithPostProcessing {
         main.doPostProcessing(new String[]{"cp -u " + folderWithConverterScripts + "/../css/calendar.css" + " " + scheduleTestDir});
 
         String library = this.getClass().getClassLoader().getResource("WithPostProcessing/lib/libical.so.1.0.1").getFile();
-        String preload = "LD_PRELOAD="+library+" ";
+        String preload = "LD_PRELOAD=" + library + " ";
         String[] command = {
-                "/bin/bash","-c",
-                preload+" "+ics2html.toString()+ " " + scheduleTestDir + " " + folderWithConverterScripts + " " + System.getProperty("PDF_START") + " " + System.getProperty("PDF_END")
+                "/bin/bash", "-c",
+                preload + " " + ics2html.toString() + " " + scheduleTestDir + " " + folderWithConverterScripts + " " + System.getProperty("PDF_START") + " " + System.getProperty("PDF_END")
         };
-        System.out.println("Command: "+command.toString());
+        System.out.println("Command: " + command.toString());
         main.doPostProcessing(command);
 
-      	try {
-    	 	//File
-     	 	File file = new File(scheduleTestDir + "allEvents.pdf");
-     	 	//Check the file is writable or read only
+        try {
+            //File
+            File file = new File(scheduleTestDir + "allEvents.pdf");
+            //Check the file is writable or read only
             if (file.exists()) {
                 file.delete();
             }
             file.createNewFile();
-            if(file.canWrite()){
-        		main.doPostProcessing(new String[]{"/bin/bash","-c","google-chrome-stable --headless --disable-gpu --print-to-pdf=" +
+            if (file.canWrite()) {
+                main.doPostProcessing(new String[]{"/bin/bash", "-c", "google-chrome-stable --headless --disable-gpu --print-to-pdf=" +
                         scheduleTestDir + "allEvents.pdf file://" + scheduleTestDir + "/allEvents.html"});
-         	}
-            else{
-     	    	System.err.println("File is read only <"+scheduleTestDir + "allEvents.pdf"+">");
-     	 	}
+            } else {
+                System.err.println("File is read only <" + scheduleTestDir + "allEvents.pdf" + ">");
+            }
 
-      	} catch (Exception e) {
-        	e.printStackTrace();
-      	} 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 //        try {
 //            Files.copy(new File(folderWithConverterScripts + "/css/calendar.css").toPath(), new File(scheduleTestDir + "/css/calendar.css").toPath(), REPLACE_EXISTING);
